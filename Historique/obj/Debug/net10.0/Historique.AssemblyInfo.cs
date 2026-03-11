8167472afc2e110ac3ea2eb30ef2bd8cd7b8be8e
Albarata <anthony.lopes.barata@gmail.com>
2026-03-11 13:54:41 +0100

    cleanup

--- Historique/obj/Debug/net10.0/Historique.AssemblyInfo.cs
+++ Historique/obj/Debug/net10.0/Historique.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Historique")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1478342c88fe94e23fe6333b724408450b5f72a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Historique")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Historique")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
